abee23bb34c15dfe29997459920e659b77f985dc
stefanandonian <stefan.andonian@gmail.com>
2019-09-24 22:18:23 -0700

    Started the app. Plan: Grid activity, settings fragment/activity, list/word activity

---
+++ app/src/main/java/mexico/view/VocabularySetGrid.kt
@@ -0,0 +1,7 @@
+package mexico.view
+
+import android.support.v7.app.AppCompatActivity
+
+class VocabularySetGrid : AppCompatActivity () {
+
+}
---
+++ app/src/main/java/mexico/view/VocabularySetGridAdapter.kt
@@ -0,0 +1,25 @@
+package mexico.view
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class VocabularySetGridAdapter : BaseAdapter {
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return
+    }
+
+    override fun getItem(p0: Int): Any {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(p0: Int): Long {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
