aa965423bccea17fefda5d57cb252a1797c40de5
stefanandonian <stefan.andonian@gmail.com>
2019-09-26 22:01:10 -0700

    Added Kotlin support, built example GridView/Adapter, and started looking into Data Binding.

--- app/src/main/java/mexico/view/VocabularySetGrid.kt
+++ app/src/main/java/mexico/view/VocabularySetGrid.kt
@@ -1,7 +1,30 @@
 package mexico.view
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
+import com.engineering.andonian.a10000spanishflashcards.R
 
 class VocabularySetGrid : AppCompatActivity () {
+
+    private val itemList: Array<String>
+        get() = arrayOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21", "Item 22")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.vocabulary_set_grid)
+
+        val gridView = findViewById<GridView>(R.id.vocabarySetGridView)
+
+        val adapter = VocabularySetGridAdapter(this, R.layout.vocabulary_set_grid_item, itemList)
+        gridView.adapter = adapter
+
+        gridView.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            Toast.makeText(this@VocabularySetGrid, " Clicked Position: " + (position + 1),
+                    Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
--- app/src/main/java/mexico/view/VocabularySetGridAdapter.kt
+++ app/src/main/java/mexico/view/VocabularySetGridAdapter.kt
@@ -1,25 +1,42 @@
 package mexico.view
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.engineering.andonian.a10000spanishflashcards.R
 
-class VocabularySetGridAdapter : BaseAdapter {
+internal class VocabularySetGridAdapter internal constructor(context: Context, private val resource: Int, private val itemList: Array<String>?) : ArrayAdapter<VocabularySetGridAdapter.ItemHolder>(context, resource) {
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        return
-    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        var holder: ItemHolder
 
-    override fun getItem(p0: Int): Any {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(resource, null)
+            holder = ItemHolder()
+            holder.name = convertView!!.findViewById(R.id.gridItemTextView)
+            holder.icon = convertView!!.findViewById(R.id.gridItemImageView)
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as ItemHolder
+        }
 
-    override fun getItemId(p0: Int): Long {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.name!!.text = this.itemList!![position]
+        holder.icon!!.setImageResource(R.mipmap.ic_launcher)
+
+        return convertView
     }
 
     override fun getCount(): Int {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return if (this.itemList != null) this.itemList.size else 0
+    }
+
+    internal class ItemHolder {
+        var name: TextView? = null
+        var icon: ImageView? = null
     }
 }
